0e1bf8c31b6093f93175a7b2972b286551a36e19
Ajeer Khan Meerankani <ajeer.meerankani@NBC02ZT2WVMD6P.EMIRATES.COM>
2022-06-07 12:58:05 +0400

    deleted compiled package

--- dist-esm/simple-echo.js
+++
@@ -1,9 +0,0 @@
-export const echo = (message) => {
-    console.log(`Received ${message} by ${Date.toString()}`);
-    return message;
-};
-export const echoV1 = (message) => {
-    console.log(`Received ${message} by ${Date.toString()}`);
-    return message;
-};
-//# sourceMappingURL=simple-echo.js.map
